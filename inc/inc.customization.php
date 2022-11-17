3c76961d39dc3d46b48e3f0ebc30f0ff525f6f19
Puk Jennekens <pukjennekens@Puks-MacBook-Pro.local>
2022-11-17 09:15:32 +0100

    Added admin styles

--- inc/inc.customization.php
+++ inc/inc.customization.php
@@ -7,3 +7,9 @@
    * @package PixelOne
    * @since 1.0.0
    */
+
+  // Admin styles
+  add_action( 'admin_enqueue_scripts', 'pixelone_admin_styles' );
+  function pixelone_admin_styles() {
+    wp_enqueue_style( 'pixelone-admin-styles', get_template_directory_uri() . '/assets/css/admin.css', PONE_CSS_DEPENDENCIES, PONE_CSS_VERSION, PONE_CSS_MEDIA );
+  }
