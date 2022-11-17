628cbb910f3a3c6568da657c93947802452b3072
Puk Jennekens <pukjennekens@Puks-MBP.localdomain>
2022-11-17 15:03:47 +0100

    Function name prefix change, all custom functions need to have the pone_ prefix

--- inc/inc.customization.php
+++ inc/inc.customization.php
@@ -9,7 +9,7 @@
    */
 
   // Admin styles
-  add_action( 'admin_enqueue_scripts', 'pixelone_admin_styles' );
-  function pixelone_admin_styles() {
+  add_action( 'admin_enqueue_scripts', 'pone_admin_styles' );
+  function pone_admin_styles() {
     wp_enqueue_style( 'pixelone-admin-styles', get_template_directory_uri() . '/assets/css/admin.css', PONE_CSS_DEPENDENCIES, PONE_CSS_VERSION, PONE_CSS_MEDIA );
   }
