291a9f2b4fab357901eebfd7d4b2eabcae94174f
Puk Jennekens <pukjennekens@Puks-MacBook-Pro.local>
2022-11-19 13:40:25 +0100

    Mobile menu setup

--- template-parts/partials/header.php
+++ template-parts/partials/header.php
@@ -47,10 +47,28 @@
       <!-- Mobile Menu -->
       <div class="lg:hidden" x-data="{open: false}">
         <button class="pone-menu-toggle" x-on:click="open = !open" x-bind:aria-expanded="open" x-bind:aria-label="open ? 'Close Menu' : 'Open Menu'" x-bind:class="open ? 'open' : ''">
-          <span class="block w-6 h-1 bg-gray-900 rounded"></span>
-          <span class="block w-6 h-1 bg-gray-900 rounded mt-1"></span>
-          <span class="block w-6 h-1 bg-gray-900 rounded mt-1"></span>
+          <span class="block w-6 h-1 bg-gray-900 rounded transition-all"></span>
+          <span class="block w-6 h-1 bg-gray-900 rounded transition-all mt-1"></span>
+          <span class="block w-6 h-1 bg-gray-900 rounded transition-all mt-1"></span>
         </button>
+
+        <div class="mobile-menu-background-overlay fixed top-0 left-0 w-screen h-screen bg-black opacity-50 z-40" x-show="open"></div>
+        <div class="mobile-menu fixed bg-white w-full h-full top-0 right-0 max-w-3xl z-50" role="navigation" x-show="open" x-on:click.away="open = false" x-bind:aria-expanded="open" x-bind:aria-label="open ? 'Close Menu' : 'Open Menu'" x-bind:class="open ? 'open' : ''">
+          <?php if( has_nav_menu( 'primary' ) ) : ?>
+            <nav role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'pixelone' ); ?>" class="py-12 px-10">
+              <?php
+                wp_nav_menu( array(
+                  'theme_location'    => 'primary',
+                  'container'         => false,
+                  'menu_class'        => 'pone-menu',
+                  'items_wrap'        => '<ul id="%1$s" class="%2$s" x-data="{open: false}">%3$s</ul>',
+                  'walker'            => new Pone_Walker_Nav_Menu(),
+                ) );
+              ?>
+            </nav>
+          <?php endif; ?>
+
+        </div>
       </div>
 
     </div>
