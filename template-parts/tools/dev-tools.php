14f06041c5f9f9f5d9ac98cb6bdf19ad5ef6ca2a
Puk Jennekens <pukjennekens@Puks-MBP.localdomain>
2022-11-22 12:31:31 +0100

    Added some server info

--- template-parts/tools/dev-tools.php
+++ template-parts/tools/dev-tools.php
@@ -11,6 +11,31 @@
    defined( 'ABSPATH' ) || die;
 ?>
 
+<?php
+    function render_table( $data, $title = '', $col_span = 1 ) {
+        ?>
+            <div class="tw-col-span-<?= $col_span; ?>">
+                <?php if( $title ): ?>
+                    <p class="mb-2">
+                        <strong><?= $title; ?></strong>
+                    </p>
+                <?php endif; ?>
+                <table class="table-auto">
+                    <tbody>
+                        <?php foreach( $data as $key => $value ): ?>
+                            <tr>
+                                <?php foreach( $value as $key => $value ): ?>
+                                    <td class="px-2 py-1 border border-gray-200"><strong><?php echo $key; ?></strong></td>
+                                    <td class="px-2 py-1 border border-gray-200"><?php echo $value; ?></td>
+                                <?php endforeach; ?>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        <?php
+    }
+?>
 
 <?php if( WP_ENV === 'development' && is_user_logged_in() ): ?>
     <div x-data="{show: true, activeTab: 1}">
@@ -34,7 +59,7 @@
                 <!-- Tab buttons -->
                 <div>
                     <button class=" px-1 hover:bg-gray-200 mr-2" x-on:click="activeTab = 1" :class="{ 'border-orange-400 border-b' : activeTab == 1  } " >
-                        Tab 1
+                        Server info
                     </button>
 
                     <button class=" px-1 hover:bg-gray-200 mr-2" x-on:click="activeTab = 2" :class="{' border-orange-400 border-b' : activeTab == 2  } ">
@@ -53,27 +78,61 @@
             </header>
 
             <!-- Tabs -->
-            <div class="container debug-menu-tab overflow-y-scroll" x-show="activeTab === 1">
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum felis rhoncus, bibendum nunc sed, sagittis dolor. Curabitur lobortis quis mauris a pretium. Quisque in ex at ex tempor iaculis ut sed nisl. Donec porttitor purus eget dui scelerisque eleifend. Nullam ornare cursus metus, at convallis ex sodales in. Aliquam feugiat magna a lectus euismod consectetur. Aliquam auctor luctus porttitor. Proin vulputate non orci sit amet placerat. Integer in est sapien. Integer ac lorem varius, mollis turpis et, pharetra ante. Mauris eleifend nisl at mauris semper, sed tristique ante tincidunt. Mauris at urna id augue luctus maximus non non nisl. Nullam pretium mauris et tincidunt egestas. Nullam non luctus lectus, molestie dignissim libero.
+            <div class="mx-4 py-4 debug-menu-tab overflow-y-scroll" x-show="activeTab === 1">
+                <div class="grid grid-cols-4 gap-4">
+                        <?php
+                            // PHP and envoirnment info
+                            $values = array(
+                                array( __( 'PHP Version', 'pixelone' ) => phpversion() ),
+                                array( __( 'Environment', 'pixelone' ) => WP_ENV ),
+                                array( __( 'Max execution time', 'pixelone' ) => ini_get( 'max_execution_time' ) ),
+                                array( __( 'Memory limit', 'pixelone' ) => ini_get( 'memory_limit' ) ),
+                                array( __( 'Upload max filesize', 'pixelone' ) => ini_get( 'upload_max_filesize' ) ),
+                                array( __( 'Post max size', 'pixelone' ) => ini_get( 'post_max_size' ) ),
+                            );
 
-                Cras sed mi at velit imperdiet commodo sit amet eget ex. Donec quis porttitor nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam accumsan, lorem eu consequat vestibulum, est nisl semper justo, sit amet tristique eros dui non massa. Aliquam eu facilisis sapien. Sed eu turpis mi. Maecenas eget sollicitudin turpis. Ut pretium nulla ac felis malesuada, at pretium risus lacinia. Mauris sit amet mattis urna, sit amet commodo lectus. Vivamus finibus risus at ipsum dignissim vestibulum. Mauris ex nisl, rutrum et lacinia at, vestibulum sed lacus. Maecenas risus lectus, lobortis eget aliquet a, congue vitae est. Vestibulum maximus maximus euismod.
+                            render_table( $values, __( 'PHP and environment info', 'pixelone' ) );
 
-                Vivamus viverra ante vel interdum consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque ac massa nec arcu tempor condimentum a ac lectus. Aliquam consequat lobortis commodo. Aliquam dignissim, risus sit amet rutrum tincidunt, orci nisl placerat ligula, at aliquam magna felis ac neque. In ante mauris, bibendum et augue vel, posuere lobortis ligula. Fusce id vestibulum nisl.
+                            // Database info
+                            global $wpdb;
 
-                Pellentesque semper enim non magna molestie, a pretium urna sagittis. Donec sed lorem ac tellus vulputate commodo in ac elit. Vestibulum ut convallis eros, vel vestibulum lectus. In malesuada posuere ipsum, in tempus mi dictum nec. Sed suscipit neque id diam dapibus sollicitudin. Maecenas et lacinia enim. Sed posuere elit non libero vulputate, ac pulvinar lorem pulvinar. Nullam tempus odio a dolor suscipit vulputate. Maecenas eget rutrum nulla. Donec vulputate sed arcu sed lacinia.
+                            $values = array(
+                                array( __( 'Database version', 'pixelone' ) => $wpdb->db_version() ),
+                                array( __( 'Database type', 'pixelone' ) => $wpdb->get_var( 'SELECT VERSION() as version' ) ),
+                                array( __( 'Database prefix', 'pixelone' ) => $wpdb->prefix ),
+                                array( __( 'Database size', 'pixelone' ) => size_format( (int) $wpdb->get_var( "SELECT SUM( data_length + index_length ) FROM information_schema.TABLES WHERE table_schema = '" . DB_NAME . "'" ) ) ),
+                                array( __( 'Database tables', 'pixelone' ) => $wpdb->get_var( "SELECT COUNT(*) FROM information_schema.TABLES WHERE table_schema = '" . DB_NAME . "'" ) ),
+                                array( __( 'Database user', 'pixelone' ) => DB_USER ),
+                                array( __( 'Database name', 'pixelone' ) => DB_NAME ),
+                            );
 
-                Cras quis erat vitae libero elementum aliquet nec at elit. Nunc faucibus dapibus sapien, id vehicula mauris pulvinar non. Duis auctor luctus nulla a accumsan. Morbi mollis pretium feugiat. Fusce eget aliquam ipsum, quis consectetur elit. Aliquam sollicitudin vehicula neque, vitae lacinia metus dapibus at. Integer felis ante, feugiat eu turpis non, sollicitudin convallis leo. Phasellus a quam nec est elementum tristique vel vel ante. Sed varius elementum elit vel volutpat. Vivamus bibendum tempus interdum. Sed eu porttitor justo. Aliquam commodo, sem nec molestie pellentesque, elit eros tempus sem, id malesuada dui risus sit amet odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent lacinia justo in finibus congue. Donec convallis in neque varius aliquet. Donec ante nunc, porttitor sed sollicitudin vitae, pretium vitae odio.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum felis rhoncus, bibendum nunc sed, sagittis dolor. Curabitur lobortis quis mauris a pretium. Quisque in ex at ex tempor iaculis ut sed nisl. Donec porttitor purus eget dui scelerisque eleifend. Nullam ornare cursus metus, at convallis ex sodales in. Aliquam feugiat magna a lectus euismod consectetur. Aliquam auctor luctus porttitor. Proin vulputate non orci sit amet placerat. Integer in est sapien. Integer ac lorem varius, mollis turpis et, pharetra ante. Mauris eleifend nisl at mauris semper, sed tristique ante tincidunt. Mauris at urna id augue luctus maximus non non nisl. Nullam pretium mauris et tincidunt egestas. Nullam non luctus lectus, molestie dignissim libero.
+                            render_table( $values, __( 'Database info', 'pixelone' ) );
 
-                Cras sed mi at velit imperdiet commodo sit amet eget ex. Donec quis porttitor nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam accumsan, lorem eu consequat vestibulum, est nisl semper justo, sit amet tristique eros dui non massa. Aliquam eu facilisis sapien. Sed eu turpis mi. Maecenas eget sollicitudin turpis. Ut pretium nulla ac felis malesuada, at pretium risus lacinia. Mauris sit amet mattis urna, sit amet commodo lectus. Vivamus finibus risus at ipsum dignissim vestibulum. Mauris ex nisl, rutrum et lacinia at, vestibulum sed lacus. Maecenas risus lectus, lobortis eget aliquet a, congue vitae est. Vestibulum maximus maximus euismod.
+                            // WordPress info
+                            $values = array(
+                                array( __( 'WordPress version', 'pixelone' ) => get_bloginfo( 'version' ) ),
+                                array( __( 'WordPress language', 'pixelone' ) => get_locale() ),
+                                array( __( 'WordPress URL', 'pixelone' ) => get_bloginfo( 'wpurl' ) ),
+                                array( __( 'WordPress home URL', 'pixelone' ) => get_bloginfo( 'url' ) ),
+                                array( __( 'WordPress theme', 'pixelone' ) => get_template() ),
+                                array( __( 'WordPress theme version', 'pixelone' ) => wp_get_theme()->get( 'Version' ) ),
+                                array( __( 'WordPress multisite', 'pixelone' ) => is_multisite() ? __( 'Yes', 'pixelone' ) : __( 'No', 'pixelone' ) ),
+                                array( __( 'WordPress debug mode', 'pixelone' ) => defined( 'WP_DEBUG' ) && WP_DEBUG ? __( 'Yes', 'pixelone' ) : __( 'No', 'pixelone' ) ),
+                                array( __( 'WordPress memory limit', 'pixelone' ) => WP_MEMORY_LIMIT ),
+                                array( __( 'WordPress timezone', 'pixelone' ) => get_option( 'timezone_string' ) ),
+                                array( __( 'WordPress permalink structure', 'pixelone' ) => get_option( 'permalink_structure' ) ),
+                                array( __( 'WordPress registered post types', 'pixelone' ) => implode( ', ', get_post_types() ) ),
+                                array( __( 'WordPress registered taxonomies', 'pixelone' ) => implode( ', ', get_taxonomies() ) ),
+                                array( __( 'WordPress registered widgets', 'pixelone' ) => implode( ', ', array_keys( $GLOBALS['wp_widget_factory']->widgets ) ) ),
+                                array( __( 'WordPress registered menus', 'pixelone' ) => implode( ', ', get_registered_nav_menus() ) ),
+                                array( __( 'WordPress registered image sizes', 'pixelone' ) => implode( ', ', get_intermediate_image_sizes() ) ),
+                                array( __( 'WordPress registered sidebars', 'pixelone' ) => implode( ', ', array_keys( $GLOBALS['wp_registered_sidebars'] ) ) ),
+                                array( __( 'WordPress registered shortcodes', 'pixelone' ) => implode( ', ', array_keys( $GLOBALS['shortcode_tags'] ) ) ),
+                            );
 
-                Vivamus viverra ante vel interdum consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque ac massa nec arcu tempor condimentum a ac lectus. Aliquam consequat lobortis commodo. Aliquam dignissim, risus sit amet rutrum tincidunt, orci nisl placerat ligula, at aliquam magna felis ac neque. In ante mauris, bibendum et augue vel, posuere lobortis ligula. Fusce id vestibulum nisl.
-
-                Pellentesque semper enim non magna molestie, a pretium urna sagittis. Donec sed lorem ac tellus vulputate commodo in ac elit. Vestibulum ut convallis eros, vel vestibulum lectus. In malesuada posuere ipsum, in tempus mi dictum nec. Sed suscipit neque id diam dapibus sollicitudin. Maecenas et lacinia enim. Sed posuere elit non libero vulputate, ac pulvinar lorem pulvinar. Nullam tempus odio a dolor suscipit vulputate. Maecenas eget rutrum nulla. Donec vulputate sed arcu sed lacinia.
-
-                Cras quis erat vitae libero elementum aliquet nec at elit. Nunc faucibus dapibus sapien, id vehicula mauris pulvinar non. Duis auctor luctus nulla a accumsan. Morbi mollis pretium feugiat. Fusce eget aliquam ipsum, quis consectetur elit. Aliquam sollicitudin vehicula neque, vitae lacinia metus dapibus at. Integer felis ante, feugiat eu turpis non, sollicitudin convallis leo. Phasellus a quam nec est elementum tristique vel vel ante. Sed varius elementum elit vel volutpat. Vivamus bibendum tempus interdum. Sed eu porttitor justo. Aliquam commodo, sem nec molestie pellentesque, elit eros tempus sem, id malesuada dui risus sit amet odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent lacinia justo in finibus congue. Donec convallis in neque varius aliquet. Donec ante nunc, porttitor sed sollicitudin vitae, pretium vitae odio.
-                </p>
+                            render_table( $values, __( 'WordPress Info', 'pixelone' ), 2 );
+                        ?>
+                </div>
             </div>
             <div class="container debug-menu-tab overflow-y-scroll" x-show="activeTab === 2" >
                 <div class="row">
