3d64ac12d6259054cab65260c1c144d5d66bd5fa
Puk Jennekens <pukjennekens@Puks-MBP.localdomain>
2022-11-23 13:46:46 +0100

    Created content partial

---
+++ template-parts/content.php
@@ -0,0 +1,12 @@
+<?php
+    defined( 'ABSPATH' ) || die;
+
+    /**
+     * The template for displaying content that is used inside loops
+     * 
+     * @package PixelOne
+     * @since 1.0.0
+     * @version 1.0.0
+     */
+?>
+    
